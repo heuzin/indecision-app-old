d900c850709e3504c59d6c92919446bbde79eb5b
heuzin <matheusqs.arq@gmail.com>
2020-07-20 20:31:16 -0400

    counter-example localStorage set up

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,26 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
         this.state = {
-            count: props.count
+            count: 0
         };
+    }
+    componentDidMount() {
+        try {
+            const json = localStorage.getItem('count')
+            const count = parseInt(json, 10)
+
+            if (!isNaN(count)) {
+                this.setState(() => ({ count }))
+            }
+        } catch (e) {
+
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            // const json = JSON.stringify(this.state.count)
+            localStorage.setItem('count', this.state.count)
+        }
     }
     handleAddOne() {
        this.setState((prevState) => {
@@ -39,10 +57,6 @@ class Counter extends React.Component {
             </div>
         )
     }
-}
-
-Counter.defaultProps = {
-    count: 0
 }
 
 ReactDOM.render(<Counter />, document.getElementById('app'))
