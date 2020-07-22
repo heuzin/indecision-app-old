0d05c5b00487bef00932d898d6c9054d8be6554e
heuzin <matheusqs.arq@gmail.com>
2020-07-22 15:04:22 -0400

    class properties transform

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
 export default class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this)
-        this.state = {
-            error: undefined
-        }
+    state = {
+        error: undefined
     }
-    handleAddOption(e) {
+    handleAddOption = (e) => {
         e.preventDefault()
 
         const option = e.target.elements.addOption.value.trim();
@@ -19,7 +15,7 @@ export default class AddOption extends React.Component {
         if (!error) {
             e.target.elements.addOption.value = '';
         }
-    }
+    };
     render() {
         return (
             <div>
